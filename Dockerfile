6cb11a5943d5871ae04c1ab03fcb43680f6a0148
Xmegan <meganli@hawaii.edu>
2022-06-19 20:24:58 -1000

    make it better

--- Dockerfile
+++ Dockerfile
@@ -4,3 +4,6 @@ WORKDIR /usr/src/app/
 
 COPY requirements.txt /usr/src/app/
 RUN pip install -r requirements.txt
+
+COPY . /usr/src/app/
+CMD script/start_app.sh
--- mysite/settings.py
+++ mysite/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ['POSTGRES_DB'],
+        'USER': os.environ['POSTGRES_USER'],
+        'PASSWORD': os.environ['POSTGRES_PASSWORD'],
+        'HOST': os.environ['DATABASE_HOST'],
+        'PORT': os.environ['DATABASE_PORT'],
     }
 }
 
