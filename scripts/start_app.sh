bf750f095b9917f5a979bbaa60667c5b21df0fe8
Xmegan <89880823+Xmegan@users.noreply.github.com>
2022-06-21 18:42:16 -1000

    make it better

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # This script starts the application
 # python manage.py runserver 0.0.0.0:8000 # using Docker requires this 0.0.0.0
-./manage.py runserver 0.0.0.0:"$PORT"
+python manage.py runserver 0.0.0.0:"$PORT"
